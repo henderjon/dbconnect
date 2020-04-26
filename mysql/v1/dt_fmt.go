895fde96a8643efcfe9179a543f92a3ff0e4153d
henderjon <list@box370.net>
2020-04-26 09:50:04 -0500

    adds DB specific DT consts

---
+++ mysql/v1/dt_fmt.go
@@ -0,0 +1,30 @@
+package mysql
+
+import (
+	"time"
+
+	"github.com/henderjon/dbconnect/datetimes"
+)
+
+const ( // Mon Jan 2 15:04:05 MST 2006
+	FmtTimestampDay    = datetimes.FmtTimestampDay
+	FmtTimestampHour   = datetimes.FmtTimestampHour
+	FmtTimestampMinute = datetimes.FmtTimestampMinute
+	FmtDateTime        = datetimes.FmtDateTime
+	FmtDate            = datetimes.FmtDate
+	FmtTime            = datetimes.FmtTime
+
+	// mysql datetime format strings
+	MySQLFmtDatetime = "%Y-%m-%d %H:%i:%s"
+	MySQLFmtMinute   = "%Y-%m-%d %H:%i"
+	MySQLFmtHour     = "%Y-%m-%d %H"
+	MySQLFmtDay      = "%Y-%m-%d"
+)
+
+func ParseMySQLDateTime(t string) time.Time {
+	timestamp, err := time.Parse(MySQLDateTime, t)
+	if err != nil {
+		panic(err)
+	}
+	return timestamp
+}
---
+++ sqlite/v1/dt_fmt.go
@@ -0,0 +1,30 @@
+package sqlite
+
+import (
+	"time"
+
+	"github.com/henderjon/dbconnect/datetimes"
+)
+
+const ( // Mon Jan 2 15:04:05 MST 2006
+	FmtTimestampDay    = datetimes.FmtTimestampDay
+	FmtTimestampHour   = datetimes.FmtTimestampHour
+	FmtTimestampMinute = datetimes.FmtTimestampMinute
+	FmtDateTime        = datetimes.FmtDateTime
+	FmtDate            = datetimes.FmtDate
+	FmtTime            = datetimes.FmtTime
+
+	// SQLite datetime format strings
+	SQLiteFmtDatetime = "%Y-%m-%d %H:%M:%S"
+	SQLiteFmtMinute   = "%Y-%m-%d %H:%M"
+	SQLiteFmtHour     = "%Y-%m-%d %H"
+	SQLiteFmtDay      = "%Y-%m-%d"
+)
+
+func ParseSQLiteDateTime(t string) time.Time {
+	timestamp, err := time.Parse(SQLiteDateTime, t)
+	if err != nil {
+		panic(err)
+	}
+	return timestamp
+}
